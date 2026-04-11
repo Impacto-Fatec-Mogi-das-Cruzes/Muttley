94d5dcd11946702edb42d65534f9e802bccafb96
Felipe Oizumi <felipe.oizumi@gmail.com>
2026-04-11 09:00:09 -0300

    first commit

---
+++ src/test/java/com/fatecmc/Muttley/MuttleyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatecmc.Muttley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuttleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
